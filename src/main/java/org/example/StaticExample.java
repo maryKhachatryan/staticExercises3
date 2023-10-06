ade15e152226f469bf6ecaae52845cbe1a0748c3
maryKhachatryan <mary.khachatryan93@gmail.com>
2023-10-07 01:08:51 +0400

    static example

--- src/main/java/org/example/StaticExample.java
+++ src/main/java/org/example/StaticExample.java
@@ -3,10 +3,12 @@ public class StaticExample {
     private static int counter = 0;
 
     public StaticExample(){
+
         counter++;
     }
     public static int getCounter() {
-       return counter;
+
+        return counter;
     }
     public static void main(String[] args){
         StaticExample object1 = new StaticExample();
